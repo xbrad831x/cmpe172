574caa2c620d30bf5a833ab84e038d49716c5561
xbrad831x <mercadobrandon33@yahoo.com>
2018-04-26 19:55:15 -0700

    Added config file

---
+++ src/configs/config_db.php
@@ -0,0 +1,45 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$conn->query("DROP DATABASE IF EXISTS 'workhub'");
+
+// Create database
+$sql = "CREATE DATABASE workhub";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
+$db = "workhub";
+$conn2 = new mysqli($servername, $username, $password, $db);
+
+$conn2->query("DROP TABLE IF EXISTS 'users'");
+
+// sql to create table
+$sql2 = "CREATE TABLE users (
+UserId VARCHAR(200), 
+Username VARCHAR(30),
+Password VARCHAR(200),
+Email VARCHAR(50))";
+
+if ($conn2->query($sql2) === TRUE) {
+    echo "Table users created successfully";
+} else {
+    echo "Error creating table: " . $conn2->error;
+}
+
+$conn->close();
+$conn2->close();
+
+?>
