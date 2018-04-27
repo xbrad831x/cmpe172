3eb7335b9aeb3ea7c3339c31ad319f1afe773770
xbrad831x <mercadobrandon33@yahoo.com>
2018-04-26 20:05:22 -0700

    Changed title

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if( isset($_SESSION['user'])!="" ){
 
 <head>
 
-<title>MyWorld</title>
+<title>Workhub</title>
 
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
