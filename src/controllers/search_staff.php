de5971af96a658e7bc55b297b39dd7be5cd6b9fa
xbrad831x <mercadobrandon33@yahoo.com>
2018-04-27 01:58:11 -0700

    Added search ui and backend for searching staff

---
+++ src/controllers/search_staff.php
@@ -0,0 +1,25 @@
+<?php
+
+$query = $_REQUEST['q'];
+
+$conn = mysqli_connect("localhost","root","","workhub");
+
+$sql = "SELECT username, email FROM users WHERE Email LIKE '%$query%'";
+
+$result = mysqli_query($conn, $sql);
+$arr = array();
+
+if(mysqli_num_rows($result) > 0)
+{
+	while($row = mysqli_fetch_assoc($result))
+	{
+		$row = mysqli_fetch_array($result, MYSQL_ASSOC);
+		$arr[] = $row;
+	}
+}
+
+echo json_encode(array_filter($arr));
+
+mysqli_close($conn);
+
+?>
--- src/views/staff_information.php
+++ src/views/staff_information.php
@@ -20,9 +20,11 @@ $row = mysqli_fetch_array($sql);
 
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
+
+<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
 
 </head>
 
@@ -36,18 +38,65 @@ $row = mysqli_fetch_array($sql);
       <div class="navbar-brand"><a href="../../index.php">Hello <?php echo $row['Username']; ?></a></div>
     </div>
     <ul class="nav navbar-nav">
-    	<li><a href="./customer_information.php">Customer Information</a></li>
-    	<li>Staff Information</li>
-    	<li><a href="./inventory.php">Inventory</a></li>
+      <li><a href="./customer_information.php">Customer Information</a></li>
+      <li>Staff Information</li>
+      <li><a href="./inventory.php">Inventory</a></li>
       <?php if(isset($_SESSION['admin'])){ ?> <li><a href="./sign_up.php">Create account</a></li> <?php }else{} ?>
-    	<li><a href="../controllers/logout_controller.php">Log Out</a></li>
+      <li><a href="../controllers/logout_controller.php">Log Out</a></li>
     </ul>
   </div>
 </nav>
+
+<div class="row">
+  <div class="col-sm-8">
+    <table id="search_table" style="display:none">
+      <tbody>
+        <thead id="table_header">
+          <tr>
+            <th>Username</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+      </tbody>
+    </table>
+  </div>
+  <div class="col-sm-4">
+    <form name="search_form" class="navbar-form navbar-left" onsubmit="return search_query()">
+      <div class="form-group">
+        <input type="text" id="search" class="form-control" placeholder="Search">
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+</div>
 
 
 </body>
 
 </div>
+
+<script type="text/javascript">
+  function search_query() {
+    var value = document.getElementById('search').value;
+    $("#search_table").show();
+    var url = 'http://localhost/src/controllers/search_staff.php?q=' + value;
+    var table = $('#search_table').DataTable( {
+    "destroy": true,
+    "processing": true,
+    "searching": false,
+    "bserverside": true,
+    "bPaginate": false,
+    "bInfo": false,
+    "sAjaxDataProp": "",
+    "sAjaxSource": url,
+    columns: [
+      {data: "username"},
+      {data: "email"}
+    ]
+  });
+
+    return false;
+}
+</script>
 
 </html>
